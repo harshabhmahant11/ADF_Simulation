ce74fc117058a36cab99925d53ee9ba715fa8ec9
dhruvarora2 <dhruvarora2@gmail.com>
2017-10-25 08:24:02 +0530

    Revert "Updated TestEventController.java"

--- FestivalPortalR1_Participant/src/com/accenture/adf/test/TestEventController.java
+++ FestivalPortalR1_Participant/src/com/accenture/adf/test/TestEventController.java
@@ -44,11 +44,6 @@ public class TestEventController {
 		/**
 		 * @TODO: Release all the objects here by assigning them null  
 		 */
-		modelAndView = null;
-		controller = null;
-		response = null;
-		request = null;
-		
 	}
 
 	/**
@@ -60,14 +55,7 @@ public class TestEventController {
 		/**
 		 * @TODO: Call getAvailableEvents methods and assert
 		 * it for appropriate model view name
-		 */	
-		try {
-			modelAndView = controller.getAvailableEvents(request, response);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			fail("Exception");
-		}
-		assertEquals("/eventCatalog.jsp", modelAndView.getViewName());
+		 */		
 	}
 
 	/**
@@ -79,14 +67,7 @@ public class TestEventController {
 		/**
 		 * @TODO: Call getAvailableEvents methods  by passing request as null
 		 * and assert it for appropriate model view name
-		 */	
-
-		try {
-			modelAndView = controller.getAvailableEvents(null, response);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			fail("Exception");
-		}
-		assertEquals(null, modelAndView.getViewName());
+		 */		
 	}
+
 }
